96bceda884de11e34f4d16cdccbb09c571604c14
HouHou01 <1207200575@qq.com>
2022-01-27 17:58:18 +0800

    updata 1.updata EVT

--- EVT/EXAM/SRC/Debug/debug.c
+++ EVT/EXAM/SRC/Debug/debug.c
@@ -168,3 +168,26 @@ __attribute__((used)) int _write(int fd, char *buf, int size)
 
     return size;
 }
+
+/*********************************************************************
+ * @fn      _sbrk
+ *
+ * @brief   Change the spatial position of data segment.
+ *
+ * @return  size: Data length
+ */
+void *_sbrk(ptrdiff_t incr)
+{
+    extern char _end[];
+    extern char _heap_end[];
+    static char *curbrk = _end;
+
+    if ((curbrk + incr < _end) || (curbrk + incr > _heap_end))
+    return NULL - 1;
+
+    curbrk += incr;
+    return curbrk - incr;
+}
+
+
+
